ec4c116f0593f069c00dc6aabd996a5cbf486639
zhaoqiju170507180208 <906721405@qq.com>
2020-04-08 11:01:24 +0800

    2020/4/8

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180208/Ch5Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180208/Ch5Activity1.java
@@ -1,5 +1,11 @@
 package cn.edu.sdwu.android.classroom.sn170507180208;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.graphics.drawable.AnimationDrawable;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -12,14 +18,12 @@ public class Ch5Activity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_ch5_1);
     }
 
     public void toast1(View view){
         Toast.makeText(this,"toast1",Toast.LENGTH_LONG).show();
-
     }
 
     public void toast2(View view){
@@ -41,5 +45,49 @@ public class Ch5Activity1 extends AppCompatActivity {
         toast.setDuration(Toast.LENGTH_SHORT);//设置时长
         toast.setGravity(Gravity.CENTER,0,0);//设置位置
         toast.show();
+    }
+    public void notification(View view){
+        Notification.Builder builder=new Notification.Builder(this);
+        //在构造器中 完成通知各种属性的设置
+        builder.setContentTitle("title");
+        builder.setContentText("message");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(android.R.drawable.ic_input_delete);//引用系统的资源 左侧可以预览
+        Notification notification=builder.build();//调用build方法 完成构造
+        //使用NotificationManager,发送通知
+        NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(101,notification);//记住编号 后续需要用到
+    }
+    public void cancel_notification(View view){
+        //使用NotificationManager 取消通知
+        NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(101);//前面标记的编号
+    }
+
+    public void alert_dialog(View view){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        //在构造器中设置对话框的属性
+        builder.setTitle("title");
+        builder.setMessage("message");
+        builder.setIcon(android.R.drawable.ic_dialog_email);
+        builder.setPositiveButton("confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"confirm",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"exit",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Ch5Activity1.this,"cancel",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
     }
 }
